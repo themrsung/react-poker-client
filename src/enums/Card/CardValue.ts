d640b470f77c187bb09fc7b3d52f6fd11e828c04
MJ Sung <themrsung@gmail.com>
2023-03-06 15:22:34 +0900

    card combination

--- src/enums/Card/CardValue.ts
+++ src/enums/Card/CardValue.ts
@@ -1,14 +1,14 @@
 enum CardValue {
-    HighCard = "High Card",
-    Pair = "Pair",
-    TwoPair = "Two Pair",
-    Triple = "Three of a Kind",
-    Straight = "Straight",
-    Flush = "Flush",
-    FullHouse = "Full House",
-    FourCard = "Four of a Kind",
-    StraightFlush = "Straight Flush",
-    RoyalStraightFlush = "Royal Straight Flush"
+    HighCard = 1,
+    Pair = 2,
+    TwoPair = 3,
+    Triple = 4,
+    Straight = 5,
+    Flush = 6,
+    FullHouse = 7,
+    FourCard = 8,
+    StraightFlush = 9,
+    RoyalStraightFlush = 10
 }
 
 const CardValueAsArray = [
